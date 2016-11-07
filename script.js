69dbc8ec574c7f40fc34db6c9aea140261506378
graholm <graholm@nuevaschool.org>
2016-11-07 09:44:15 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -3,19 +3,29 @@ function logArgs(args) {
   console.log(args)
 }
 
-function console() {
-var i_am_also_going_to_log_this_in_my_console = 20
-console.log ("this is what i am console logging")
-console.log (i_am_also_going_to_log_this_in_my_console)
-}
-
 var functionString = logArgs.toString()
 
-var functiongExample = $("#functionexample")
+var consoleLogExample = $("#console-log-example")
+
 var formattedExample = $("<pre>")
-var consolelogexample = $ ("#console-log-example")
 
 formattedExample.text(functionString)
 
-consolelogexample.text(consolelogString)
-functionExample.html(formattedExample)
+consoleLogExample.html(formattedExample)
+
+//function with argument
+function perimeter(length, width) {
+  console.log(length + length + width + width);
+};
+
+perimeter(3,5);
+
+var functionString = perimeter.toString()
+
+var consoleLogExample = $("#function-example")
+
+var formattedExample = $("<pre>")
+
+formattedExample.text(functionString)
+
+consoleLogExample.html(formattedExample)
