bf3f46d38e3f8dde633242cd10c90c1d2d8a1fe6
graholm <graholm@nuevaschool.org>
2016-11-07 09:12:39 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -3,15 +3,14 @@ function logArgs(args) {
   console.log(args)
 }
 
+function console() {
 var i_am_also_going_to_log_this_in_my_console = 20
 console.log ("this is what i am console logging")
 console.log (i_am_also_going_to_log_this_in_my_console)
+}
 
 var functionString = logArgs.toString()
-
-
-var functionString = logArgs.toString()
-
+var functionString = console.toString()
 
 var consoleLogExample = $("#console-log-example")
 
