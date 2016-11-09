017e23a3f77e6939459f80cff8dbb43d0a46cb10
graholm <graholm@nuevaschool.org>
2016-11-09 08:50:21 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -4,28 +4,28 @@ function logArgs(args) {
 }
 
 var functionString = logArgs.toString()
-
 var consoleLogExample = $("#console-log-example")
-
 var formattedExample = $("<pre>")
-
 formattedExample.text(functionString)
-
 consoleLogExample.html(formattedExample)
 
-//function with argument
+
+
+
+
 function perimeter(length, width) {
   console.log(length + length + width + width);
 };
 
-var call = "\nperimeter(3,5)"
 
-var functionString = perimeter.toString() + call
+function whatsup(args) {
+  var args = 1
+  console.log(args)
+};
 
+
+var functionString = whatsup.toString()
 var consoleLogExample = $("#function-example")
-
 var formattedExample = $("<pre>")
-
 formattedExample.text(functionString)
-
 consoleLogExample.html(formattedExample)
