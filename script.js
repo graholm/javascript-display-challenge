cf7f390b59f9add5a4db99a4a3a5d94a03620878
graholm <graholm@nuevaschool.org>
2016-11-09 09:00:42 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -4,66 +4,28 @@ function logArgs(args) {
 }
 
 var functionString = logArgs.toString()
-
 var consoleLogExample = $("#console-log-example")
-
 var formattedExample = $("<pre>")
-
 formattedExample.text(functionString)
-
 consoleLogExample.html(formattedExample)
 
-//function with argument
+
+
+
+
 function perimeter(length, width) {
   console.log(length + length + width + width);
 };
 
-var call = "\nperimeter(3,5)"
 
-var functionString = perimeter.toString() + call
+function whatsup(args) {
+  var args = 1
+  console.log(args)
+};
 
-var functionExample = $("#function-example")
 
+var functionString = whatsup.toString()
+var consoleLogExample = $("#function-example")
 var formattedExample = $("<pre>")
-
 formattedExample.text(functionString)
-
-functionExample.html(formattedExample)
-
-//function takes in function
-
-
-
-//jquery
-var button = $("#button").click(function() {
-  console.log("Jquery Example");
-});
-
-$(document).ready(function() {
-  $('#colorblock').mouseenter(function() {
-    $(this).animate({
-      height: '+=10px'
-    });
-  });
-  $('#colorblock').mouseleave(function() {
-    $(this).animate({
-      height: '-=10px'
-    }); 
-  });
-  $('#colorblock').click(function() {
-    $(this).toggle(1000);
-  }); 
-});
-
-var functionString = button.toString()
-
-var buttonExample = $("#button")
-
-var formattedExample = $("<pre>")
-
-formattedExample.text(functionString)
-
-buttonExample.html(formattedExample)
-
-//firebase
-
+consoleLogExample.html(formattedExample)
