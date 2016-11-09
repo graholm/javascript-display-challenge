c16d7eb76737f1770fcf9de3cf16a366daf79ac1
graholm <graholm@nuevaschool.org>
2016-11-09 09:14:33 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -31,8 +31,16 @@ formattedExample.text(functionString)
 functionExample.html(formattedExample)
 
 //function takes in function
-firebase.get("variable", function(result) {}
+function get("variable", function(result) {
+  
+}
 
+function get() {
+  setTimeout(function() {
+    callback(1)
+}, 1000)
+}
+//get is a function
 
 //jquery
 var button = $("#button").click(function() {
