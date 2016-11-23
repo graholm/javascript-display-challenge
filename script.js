5091e22755426c6682c67a83efc394a08dba047c
graholm <graholm@nuevaschool.org>
2016-11-23 08:32:33 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,43 +1,84 @@
-//console.log
 function logArgs(args) {
   var args = 1
   console.log(args)
 }
 
 var functionString = logArgs.toString()
+
 var consoleLogExample = $("#console-log-example")
+
 var formattedExample = $("<pre>")
+
 formattedExample.text(functionString)
+
 consoleLogExample.html(formattedExample)
 
-//function
-function myfunction(a, b) {
-  return a * b;
-}
-myfunction(4, 3);
-var call = "12"
-var functionString = myfunction.toString() + call
-var functionExample = $("#function-example")
-var formattedExample = $("<pre>")
-formattedExample.text(functionString)
-functionExample.html(formattedExample)
-
-
-
-
-
+//function with argument
 function perimeter(length, width) {
   console.log(length + length + width + width);
+  
 };
 
+var call = "\nperimeter(3,5)"
 
-function whatsup(args) {
-  var args = 1
-  console.log(args)
-};
+var functionString = perimeter.toString() + call
 
-var functionString = whatsup.toString()
-var consoleLogExample = $("#notfunction-example")
+var functionExample = $("#function-example")
+
 var formattedExample = $("<pre>")
+
 formattedExample.text(functionString)
-consoleLogExample.html(formattedExample)
+
+functionExample.html(formattedExample)
+
+//function takes in function
+$("#function-input").keydown(function(e) {	
+  if (e.keyCode == "13") {
+    $("#function-input").css("background-color", "yellow");
+  }
+})
+
+//jquery
+$("#button").click(function() {
+  console.log("Jquery Example");
+});
+
+$(document).ready(function() {
+  $('#colorblock').mouseenter(function() {
+    $(this).animate({
+      height: '+=10px'
+    });
+  });
+  $('#colorblock').mouseleave(function() {
+    $(this).animate({
+      height: '-=10px'
+    }); 
+  });
+  $('#colorblock').click(function() {
+    $(this).toggle(1000);
+  }); 
+});
+
+var functionString = button.toString()
+
+var buttonExample = $("#button")
+
+var formattedExample = $("<pre>")
+
+formattedExample.text(functionString)
+
+buttonExample.html(formattedExample)
+
+//firebase
+var input = $("#firebase-input")
+
+input.keydown(function(e) {	
+  if (e.keyCode == "13") {
+    var valueToSave = input.val();
+    addListItem("sophiasList", valueToSave);
+  }
+})
+
+onNewListItem("sophiasList", function(value) {
+  $("#firebase-display").append("<div>" + value + "</div>")
+}) 
